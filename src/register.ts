b9251019bf2ad92542fde24b5d06059068455a1e
MatthieuCoder <matthieu@matthieu-dev.xyz>
2023-01-05 21:34:34 +0400

    change user depending on the rest token in the rest component

--- src/register.ts
+++ src/register.ts
@@ -1,14 +1,18 @@
-import 'source-map-support';
-import {REST} from '@discordjs/rest';
-import {commands} from './commands';
-import {registerCommands} from './sys/handler';
+import "source-map-support";
+import { REST } from "@discordjs/rest";
+import { commands } from "./commands";
+import { registerCommands } from "./sys/handler";
+import { RESTGetAPIUserResult, Routes } from "discord-api-types/v10";
 
 const rest = new REST({
-	version: '10',
-	api: 'http://localhost:8090/api',
-}).setToken('_');
+  version: "10",
+  api: "http://localhost:8090/api",
+}).setToken("_");
 
 /**
  * We register the commands with discord
  */
-(async () => registerCommands(commands, rest, '807188335717384212'))();
+(async () => {
+  let self = (await rest.get(Routes.user("@me"))) as RESTGetAPIUserResult;
+  registerCommands(commands, rest, self.id);
+})();

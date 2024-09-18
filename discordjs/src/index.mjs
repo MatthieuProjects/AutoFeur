698c6b62c1882fc013f3ee9c85ef85813af2e3ef
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2024-09-18 21:16:21 +0400

    Update index.mjs

--- discordjs/src/index.mjs
+++ discordjs/src/index.mjs
@@ -79,7 +79,7 @@ const messageAction = async (message) => {
     }
   }
   
-  if (message.cleanContent.includes("@everyone") && !ignoredEveryoneChannels.includes(message.channelId)) {
+  if (message.content.includes("@everyone") && !ignoredEveryoneChannels.includes(message.channelId)) {
     message.reply("https://cdn.mpgn.dev/pascontent-gabe.gif");
   }
   if (message.content.includes("<:quoi:1061204752542748742>")) {
